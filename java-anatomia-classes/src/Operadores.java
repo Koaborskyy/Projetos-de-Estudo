c02c45f76ee10f26fa40964a363cccf5c888cc36
Koaborskyy <tavinhogames@gmail.com>
2024-09-18 23:37:26 -0300

    Update
    seguindo o curso!

--- java-anatomia-classes/src/Operadores.java
+++ java-anatomia-classes/src/Operadores.java
@@ -1,6 +1,6 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "Linguagem" + "JAVA";
+       /* String nomeCompleto = "Linguagem" + "JAVA";
         System.out.println(nomeCompleto);
 
         String concatenacao = "?";
@@ -25,7 +25,35 @@ public class Operadores {
         // numero = + numero; não funciona pois o sinal de + vai fazer a concatenação e teria que fazer conforme abaixo multiplicando por -1
         numero = numero * -1;
         System.out.println(numero);
-    
+     
 
+     int numero1 = 2;
+     int numero2 = 2;
+
+     boolean simNao = numero1 == numero2;
+
+     if (simNao) {
+        System.out.println("Numero 1 é igual ao numero 2.");
+        
+     } else { 
+        System.out.println("Numero 1 é diferente do numero 2.");
     }
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+        
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são veradeiras");
+        } else {
+            System.out.println("As duas condições não são veradeiras");       
+         }
+        
+         if (condicao1 || condicao2) {
+            System.out.println("Uma das duas condições são verdadeiras");
+            
+         } else {
+            System.out.println("Nenhuma das condições são verdadeiras!");
+         }
+
+        */
+    } 
 }
