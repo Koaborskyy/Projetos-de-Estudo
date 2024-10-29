5e150d1e379f8502f97c4efc35e11352095a9996
Octavio Diniz <Octavio.diniz@codemar-sa.com.br>
2024-10-29 11:54:00 -0300

    atualizacao DIO

---
+++ java-anatomia-classes/src/ClasseEntendoJava.java
@@ -0,0 +1,14 @@
+public class ClasseEntendoJava {
+    public static void main (String[] args) {
+       // System.out.print( "Bem vindo ao curso de java bootcamp claro!");
+
+       String primeiroNome = "Octávio";
+       String segundoNome = "Diniz";
+
+       String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+       System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
